441def6571ed71f7227109149b395b8fdc801399
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2020-12-06 11:44:54 -0300

    Add clause to handle invalid input

--- lib/chapter_3_control_flow/geometry.ex
+++ lib/chapter_3_control_flow/geometry.ex
@@ -10,4 +10,12 @@ defmodule Geometry do
   def area({:circle, r}) do
     r * r * 3.14
   end
+
+  def area(shape_not_known) do
+    {:error, {:unknown_shape, shape_not_known}}
+  end
+
+  def area() do
+    {:error, :shape_not_provided}
+  end
 end
