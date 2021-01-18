2ae1681f77ec404b81bb0f347133117b39e3d938
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2021-01-18 09:15:07 -0300

    Add high order function example

---
+++ lib/chapter_3_control_flow/high_order_functions.ex
@@ -0,0 +1,10 @@
+defmodule HighOrderFunctions do
+  def extract_user(user) do
+    case Enum.filter(
+      ["login", "email", "password"],
+      fn key -> Map.has_key?(user,key) end) do
+      [] -> IO.puts("No missing fileds")
+      missing_fields -> Enum.each(missing_fields, fn missing_field -> IO.puts(missing_field) end)
+    end
+  end
+end
