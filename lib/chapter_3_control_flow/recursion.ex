e99bfb224a64432ce5b2254c03b93a3591e32a68
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2021-01-18 07:22:27 -0300

    Add sum example

--- lib/chapter_3_control_flow/recursion.ex
+++ lib/chapter_3_control_flow/recursion.ex
@@ -1,7 +1,12 @@
-defmodule NaturalNumbers do
+defmodule Recursion do
   def print(1), do: IO.puts(1)
   def print(n) do
     print(n-1)
     IO.puts(n)
+  end
+
+  def sum([]), do: 0
+  def sum([head | tail]) do
+    head + sum(tail)
   end
 end
