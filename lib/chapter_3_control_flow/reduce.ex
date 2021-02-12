4b47ae55a27bb712bba71e711922c683d82c6f65
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2021-02-12 10:14:01 -0300

    Add Reduce example

---
+++ lib/chapter_3_control_flow/reduce.ex
@@ -0,0 +1,14 @@
+defmodule Reduce do
+  def reduce_by_type() do
+    Enum.reduce([1,"not a number", 2, :x, 3],
+    0,
+    fn
+      element, sum when is_number(element) ->
+        sum + element
+
+      _, sum -> sum
+    end
+    )
+  end
+
+end
