a3bfa852eec48c43c93a529d861186a63ef4cd7a
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2021-02-15 11:36:39 -0300

    Create TodoList module

---
+++ lib/chapter_4_data_abstractions/todo_list.ex
@@ -0,0 +1,10 @@
+defmodule TodoList do
+  @moduledoc """
+  The to-do list will support the following features:
+  - Creating a new data abstraction
+  - Adding new entries
+  - Querying the abstraction
+  """
+
+  def new(), do: %{}
+end
