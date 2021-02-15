f6a293d2a3f275e6244427f55501d7037abcdb6f
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2021-02-15 11:35:49 -0300

    Add MapSet example

---
+++ lib/chapter_4_data_abstractions/map_set_ex.ex
@@ -0,0 +1,11 @@
+defmodule MapSetEx do
+  def instance_and_modify() do
+      MapSet.new()
+      |> MapSet.put(:monday)
+      |> MapSet.put(:tuesday)
+  end
+
+  def query(days, day) do
+    MapSet.member?(days, day)
+  end
+end
