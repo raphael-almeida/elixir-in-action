807f959f08ea4ef340f443fccd9e4c9c9cfc8a65
Raphael Almeida <raphael.augusto.almeida@gmail.com>
2021-02-26 09:11:22 -0300

    Structure data with Map

--- lib/chapter_4_data_abstractions/map_set_ex.ex
+++ lib/chapter_4_data_abstractions/map_set_ex.ex
@@ -1,8 +1,8 @@
 defmodule MapSetEx do
   def instance_and_modify() do
-      MapSet.new()
-      |> MapSet.put(:monday)
-      |> MapSet.put(:tuesday)
+    MapSet.new()
+    |> MapSet.put(:monday)
+    |> MapSet.put(:tuesday)
   end
 
   def query(days, day) do
--- lib/chapter_4_data_abstractions/todo_list.ex
+++ lib/chapter_4_data_abstractions/todo_list.ex
@@ -7,4 +7,12 @@ defmodule TodoList do
   """
 
   def new(), do: %{}
+
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
+  end
+
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
+  end
 end
